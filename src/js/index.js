24844dafe1545c6edfb17c49801ca6b807a32ff4
Pranav Sharma <88233021+Cyberpunkpranav@users.noreply.github.com>
2023-05-27 19:24:00 +0530

    27 may 7:23pm

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,52 @@
-// Get the navbar
 var navbar = document.getElementById("navbar");
 
-// Get the initial position of the navbar
-var navbarPosition = navbar.offsetTop;
+let prevScrollPos = window.pageYOffset;
+window.onscroll = function () {
+    const currentScrollPos = window.pageYOffset;
 
-// Function to hide the navbar on scroll
-function hideNavbarOnScroll() {
-    if (window.pageYOffset > navbarPosition) {
-        navbar.style.top = "-50px";
-    } else {
+    if (prevScrollPos > currentScrollPos) {
         navbar.style.top = "0";
+    } else {
+        navbar.style.top = `-${navbar.offsetHeight}px`;
     }
-}
 
-// Add event listener to window for scroll event
-window.addEventListener("scroll", hideNavbarOnScroll);
+    prevScrollPos = currentScrollPos;
+};
 
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
     document.body.style.overflow = 'auto';
     document.getElementById('content').style.paddingRight = window.innerWidth - document.documentElement.clientWidth + 'px';
-    
+
 })
+// window.addEventListener('scroll', AnimateDiv)
+// function AnimateDiv() {
+//     var pageHeight = window.innerHeight
+//     var Scroll = window.pageYOffset
+//     var resultHeight = pageHeight + Scroll
+//     console.log(pageHeight, Scroll)
+//     if(resultHeight >pageHeight){
+//         $('.banner1').css("height", (resultHeight, window.pageYOffset)+``)
+//         $('.banner1').css("width", (resultHeight, window.pageYOffset))
+//         $('.banner1').css("left", ((pageHeight - Scroll)))
+//         $('.banner1').css("top", (pageHeight - Scroll))
+//         $('.banner1').css('background-color','#000')
+//     }
+
+
+// }
+
+// var expandDiv = document.querySelector('.banner1');
+// var initialHeight = 0; 
+// var scrollRange = 500; 
+// var maxExpand = 150; 
+// var initialWidth = 0;
+// window.addEventListener('scroll', function () {
+//     var scrollPosition = window.scrollY;
+//     var expandAmount = Math.min(scrollPosition / scrollRange * maxExpand, maxExpand);
+//     var newHeight = initialHeight + expandAmount;
+//     var newWidth = initialWidth + expandAmount;
+//     expandDiv.style.height = newHeight + 'rem';
+//     expandDiv.style.width = newWidth + 'rem';
+//     expandDiv.css('background-color', '#000')
+
+// });
