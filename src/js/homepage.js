e8967896684558388877ce9589841836a7b7c323
Pranav Sharma <88233021+Cyberpunkpranav@users.noreply.github.com>
2023-05-30 17:51:05 +0530

    30 may 5:50pm

--- src/js/homepage.js
+++ src/js/homepage.js
@@ -6,28 +6,41 @@
 //         $('.heading1').css('height', `${divWidth}rem`)
 //         $('.heading2').css('top', `-${divWidth}rem`)
 // })
-var divWidth = window.pageYOffset
-console.log(divWidth);
+
 window.addEventListener('scroll', function () {
 
-    // console.log(window.innerWidth, window.innerHeight)
+
     var divWidth = window.pageYOffset
-    if (window.pageYOffset>400){
-        $('.slide1').css('height', `0px`)
-        $('.divider').css('height', `${(window.pageYOffset+100)-(window.pageYOffset)}vh`)
-        $('.hr').css('display','none')
-        $('.slide2').css('height', `0px`)
-    }else{
+    if (window.pageYOffset < 500) {
         $('.slide1').css('height', `60vh`)
+        $('.divider').css('height', `00vh`)
         $('.slide2').css('height', `60vh`)
     }
-    if(window.pageYOffset<700){
-        $('.divider').css('height', `30vh`)
+    if (window.pageYOffset > 500) {
+        $('.slide1').css('height', `55vh`)
+        $('.divider').css('height', `20vh`)
+        $('.hr').css('display', 'none')
+        $('.slide2').css('height', `55vh`)
     }
-    if (window.pageYOffset<500){
-        $('.divider').css('height', `0vh`)
-        $('.hr').css('display','block')
+    if (window.pageYOffset > 600) {
+        $('.slide1').css('height', `40vh`)
+        $('.slide2').css('height', `40vh`)
+        $('.divider').css('height', `50vh`)
+        $('.hr').css('display', 'block')
+    }
+    if (window.pageYOffset > 1000) {
+        $('.slide1').css('height', `30vh`)
+        $('.divider').css('height', `70vh`)
+        $('.slide2').css('height', `30vh`)
+    }
+    if (window.pageYOffset > 1200) {
+        $('.slide1').css('height', `0vh`)
+        $('.divider').css('height', `100vh`)
+        $('.slide2').css('height', `0vh`)
     }
     console.log(divWidth)
-       
+
 })
+// else {
+
+// }
