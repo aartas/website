3ca7f822d61d56c98fae8bc8d70768b0191f9ee2
Pranav Sharma <88233021+Cyberpunkpranav@users.noreply.github.com>
2023-05-30 18:28:14 +0530

    30 may 6:28pm

--- src/js/homepage.js
+++ src/js/homepage.js
@@ -11,39 +11,39 @@ window.addEventListener('scroll', function () {
 
 
     var divWidth = window.pageYOffset
-    if (window.pageYOffset < 300) {
+    if (window.pageYOffset < 100) {
         $('.slide1').css('height', `60vh`)
         $('.divider').css('height', `0vh`)
         $('.slide2').css('height', `60vh`)
     }
-    if (window.pageYOffset > 400) {
+    if (window.pageYOffset > 200) {
         $('.slide1').css('height', `50vh`)
         $('.divider').css('height', `10vh`)
         $('.hr').css('display', 'none')
         $('.slide2').css('height', `55vh`)
     }
-    if (window.pageYOffset > 500) {
+    if (window.pageYOffset > 300) {
         $('.slide1').css('height', `40vh`)
         $('.divider').css('height', `30vh`)
         $('.slide2').css('height', `40vh`)
         $('.hr').css('display', 'block')
     }
-    if (window.pageYOffset > 600) {
+    if (window.pageYOffset > 400) {
         $('.slide1').css('height', `30vh`)
         $('.divider').css('height', `50vh`)
         $('.slide2').css('height', `30vh`)
     }
-    if (window.pageYOffset > 700) {
+    if (window.pageYOffset > 500) {
         $('.slide1').css('height', `20vh`)
         $('.divider').css('height', `60vh`)
         $('.slide2').css('height', `40vh`)
     }
-    if (window.pageYOffset > 800) {
+    if (window.pageYOffset > 600) {
         $('.slide1').css('height', `20vh`)
         $('.divider').css('height', `70vh`)
         $('.slide2').css('height', `50vh`)
     }
-    if (window.pageYOffset > 900) {
+    if (window.pageYOffset > 700) {
         $('.slide1').css('height', `15vh`)
         $('.divider').css('height', `80vh`)
         $('.slide2').css('height', `60vh`)
