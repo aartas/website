9e47679fd349dae5c16042c877b8dde4c7bad09f
Pranav Sharma <88233021+Cyberpunkpranav@users.noreply.github.com>
2023-05-30 18:07:17 +0530

    30 may 6:07pm

--- src/js/homepage.js
+++ src/js/homepage.js
@@ -11,32 +11,47 @@ window.addEventListener('scroll', function () {
 
 
     var divWidth = window.pageYOffset
-    if (window.pageYOffset < 500) {
+    if (window.pageYOffset < 300) {
         $('.slide1').css('height', `60vh`)
-        $('.divider').css('height', `00vh`)
+        $('.divider').css('height', `0vh`)
         $('.slide2').css('height', `60vh`)
     }
-    if (window.pageYOffset > 500) {
-        $('.slide1').css('height', `55vh`)
-        $('.divider').css('height', `20vh`)
+    if (window.pageYOffset > 400) {
+        $('.slide1').css('height', `50vh`)
+        $('.divider').css('height', `10vh`)
         $('.hr').css('display', 'none')
         $('.slide2').css('height', `55vh`)
     }
-    if (window.pageYOffset > 600) {
+    if (window.pageYOffset > 500) {
         $('.slide1').css('height', `40vh`)
+        $('.divider').css('height', `30vh`)
         $('.slide2').css('height', `40vh`)
-        $('.divider').css('height', `50vh`)
         $('.hr').css('display', 'block')
     }
-    if (window.pageYOffset > 1000) {
+    if (window.pageYOffset > 600) {
         $('.slide1').css('height', `30vh`)
-        $('.divider').css('height', `70vh`)
+        $('.divider').css('height', `50vh`)
         $('.slide2').css('height', `30vh`)
     }
-    if (window.pageYOffset > 1200) {
+    if (window.pageYOffset > 700) {
+        $('.slide1').css('height', `20vh`)
+        $('.divider').css('height', `60vh`)
+        $('.slide2').css('height', `40vh`)
+    }
+    if (window.pageYOffset > 800) {
+        $('.slide1').css('height', `20vh`)
+        $('.divider').css('height', `70vh`)
+        $('.slide2').css('height', `50vh`)
+    }
+    if (window.pageYOffset > 900) {
+        $('.slide1').css('height', `15vh`)
+        $('.divider').css('height', `80vh`)
+        $('.slide2').css('height', `60vh`)
+    }
+    if (window.pageYOffset > 1000) {
         $('.slide1').css('height', `0vh`)
         $('.divider').css('height', `100vh`)
-        $('.slide2').css('height', `0vh`)
+        $('.slide2').css('height', `60vh`)
     }
     console.log(divWidth)
 
