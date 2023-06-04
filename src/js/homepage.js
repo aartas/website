b9b3db3d49fce2e8cbc75b806bb654d31e8cfaca
Pranav Sharma <88233021+Cyberpunkpranav@users.noreply.github.com>
2023-06-04 14:34:21 +0530

    4 Jun 2:34pm

--- src/js/homepage.js
+++ src/js/homepage.js
@@ -1,63 +1,131 @@
 
-gsap.registerPlugin(ScrollTrigger)
-gsap.defaults({
-  markers: true,
-  ease: 'in-ease',
-  duration: 1
+// gsap.registerPlugin(ScrollTrigger)
+// gsap.defaults({
+//   markers: true,
+//   ease: 'in-ease',
+//   duration: 1
+// })
+// const tl = gsap.timeline();
+// tl.fromTo(".slide1", {
+//   height: 0,
+//   opacity: 0.8
+// }, {
+//   height: "100vh",
+//   paddingLeft: '20vh',
+//   paddingTop: '35vh',
+//   opacity: 1
+// })
+// .from('.slide2', { yPercent: 170,duration:5 })
+//   .from('.slide3', { yPercent: 170,duration:5 })
+//   .from('.slide4', { yPercent: 170,duration:5 })
+
+
+// ScrollTrigger.create({
+//   animation: tl,
+//   markers: true,
+//   trigger: '.slides',
+//   start: '-300vh top',
+//   end: " top",
+//   scrub: 2,
+//   pin: '.slides',
+//   pinSpacing: false,
+
+// })
+
+
+// const t1 = gsap.timeline()
+// t1.to('.slide0a', {
+//   yPercent: -100,
+//   duration: 1
+// }).to('.slide0b', {
+//   yPercent: 10,
+//   duration: 1
+// })
+
+// gsap.to('.slide1', {
+//   height: '100vh',
+//   yPercent: -50,
+//   duration: 1
+// })
+
+gsap.to('.scroller', {
+  xPercent: -100,
+  width: '100%',
+  height: '100vh',
+  duration: 12,
+  scrollTrigger: {
+    start: 'top top',
+    end: '+=2500vh bottom',
+    pin: true,
+    scrub: 1,
+    trigger: '.specialities',
+    toggleActions: 'play none none none'
+  }
 })
-const tl = gsap.timeline();
-tl.fromTo(".slide1", {
-  height: 0,
-  opacity: 0.8
-}, {
-  height: "100vh",
-  paddingLeft: '20vh',
-  paddingTop: '35vh',
-  opacity: 1
-}).from('.slide2', { yPercent: 150 })
-  .from('.slide3', { yPercent: 150 })
-  .from('.slide4', { yPercent: 150 })
 
+gsap.to('.scroll-thumb', {
+  y: '80vh',
+  duration: 12,
+  scrollTrigger: {
+    start: 'top top',
+    end: '+=2000vh',
+    scrub: 1,
+    trigger: '.scroller',
+    toggleActions: 'play none none none'
+  }
+})
 
+const t2 = gsap.timeline()
+t2.to('#appfeature1', {
+  xPercent: -140,
+  duration: 5,
+  height: '60vh',
+  width: '28vh',
+  delay: 1,
+}).to('#appheadline1', {
+  opacity: 0,
+}).to('#appheadline2', {
+  duration: 10,
+  opacity: 1,
+  duration: 2,
+}).to('#appfeature2', {
+  xPercent: -278,
+  duration: 10,
+  height: '60vh',
+  width: '28vh',
+}).to('#appheadline2', {
+  opacity: 0
+}).to('#appheadline3', {
+  opacity: 1,
+  duration: 2
+}).to('#appfeature3', {
+  xPercent: -350,
+  duration: 1,
+  height: '50vh',
+  width: '20vh',
+  duration: 10,
+}).to('#appheadline3', {
+  opacity: 0,
+}).to('#appheadline4', {
+  opacity: 1,
+  duration: 2,
+}).to('#appfeature3', {
+  xPercent: -450,
+  height: '61vh',
+  width: '29vh',
+  duration: 5,
+})
 ScrollTrigger.create({
-  animation: tl,
+  animation: t2,
+  start: '-100vh top',
   markers: true,
-  trigger: '.slides',
-  start: 'top top',
-  end: "100vh",
-  scrub: 2,
-  pin: '.slides',
+  end: '+=12000vh',
+  pin: '.aartasapp',
   pinSpacing: false,
-  anticipatePin: 1,
+  scrub: 1,
+  trigger: '.phone',
+  toggleActions: 'play none none none'
 
 })
-gsap.to('.scroller',{
-  xPercent:-60,
-  duration:12,
-  scrollTrigger:{
-    markers:true,
-    start:'top top',
-    end:'+=2000vh',
-    pin:true,
-    scrub:1,
-    trigger:'.specialities',
-    toggleActions:'play restart reverse none'
-  }
-})
-
-gsap.to('.scroll-thumb',{
-  y:'85vh',
-  duration:12,
-  scrollTrigger:{
-    markers:true,
-    start:'top top',
-    end:'+=2000vh',
-    pin:true,
-    scrub:1,
-    trigger:'.scroller',
-    toggleActions:'play restart reverse none'
-  }
-})
-
 
 
